1a9d933f3b15bcc1967f260398acdb5df6b9d9e8
Rathinavelkumar <Rathinavelkumar.Murugan@gmail.com>
2021-11-04 21:36:45 +0530

    updated code

--- Top_Interview_Questions_EasyCollection/Strings/ReverseInteger.py
+++ Top_Interview_Questions_EasyCollection/Strings/ReverseInteger.py
@@ -1,16 +1,16 @@
-x = 1563847412
-
-sum=0
-temp=abs(x)
-while temp>0:
-    sum = (sum*10) + (temp%10)
-    temp //= 10
-
-if sum >= -(2**31) and sum <= ((2**31)-1):
-    if x<0:
-        sum = -1 * sum
-        print(sum)
-    else:
-        print(sum)
-else:
-    print(0)
+class Solution:
+    def reverse(self, x: int) -> int:
+        sum = 0
+        temp = abs(x)
+        while temp>0:
+            sum = (sum*10) + (temp%10)
+            temp = temp//10
+        
+        if sum >= -(2**31) and sum <= ((2**31)-1):
+            if x<0:
+                sum = -1*sum
+                return sum
+            else:
+                return sum
+        else:
+            return 0
