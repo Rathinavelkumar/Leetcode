868a49ce7d843ac3cdefbd1f031974fee970a5f1
Rathinavelkumar <Rathinavelkumar.Murugan@gmail.com>
2021-11-08 20:47:37 +0530

    updated code

--- Top_Interview_Questions_EasyCollection/Strings/StringtoInteger.py
+++ Top_Interview_Questions_EasyCollection/Strings/StringtoInteger.py
@@ -1,43 +1,35 @@
-s = "-0000042a1234"
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        data = s.strip().split(" ")[0]
 
-if " " in s.strip():
-    data = s.strip().split(" ")[0]
-else:
-    data = s
-result=[]
-sign=[]
-int_flag=False
-for each in data:
-    if each=='+' or each=='-':
-        if len(sign)!=0:
-            break
-        else:
-            if int_flag==False:
-                sign.append(each)
-                continue
-            else:
+        sign=[]
+        result=[]
+        active_flag=False
+        for each in data:
+            if (each=='+' or each=="-") and len(sign)==0:
+                if active_flag==False:
+                    sign.append(each)
+                    continue
+                else:
+                    break
+            if not each.isnumeric():
                 break
-    if each.isdigit():
-        int_flag=True
-        if each=='0' and len(result)==0:
-            continue
-        else:
-            result.append(each)
-    if int_flag==True and not each.isdigit():
-        if each=='+' or each=='-':
-            result=[]
-            break
-        else:
-            break
+            else:
+                active_flag=True
+                if len(result)==0 and each=='0':
+                    continue
+                else:
+                    result.append(each)
 
-if len(result)>0:
-    sign.extend(result)
-    output = int("".join(sign))
-    if output < -2**31:
-        print(-2**31)
-    elif output > (2**31)-1:
-        print((2**31)-1)
-    else:
-        print(output)
-else:
-    print(0)
+
+        if len(result)>0:
+            sign.extend(result)
+            output = int("".join(sign))
+            if output < -2**31:
+                return(-2**31)
+            elif output > (2**31)-1:
+                return((2**31)-1)
+            else:
+                return(output)
+        else:
+            return(0)
